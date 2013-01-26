40a265a4e8e8ca3e1c2deaf67deab61fc6722d63
alisdair muircroft <alisdairmuircroft@hotmail.com>
2013-01-26 04:39:11 +0000

    freddy added

---
+++ TheAmazingSMADMen/Assets/Scripts/Movement.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class Movement : MonoBehaviour {
+	
+	
+	public AudioClip heart;
+	public AudioClip heartFast;
+	public GameObject prey;
+	
+	public float audioFarDistance = 15.0f;
+	public float audioNearDistance = 6.0f;
+	private float distance;
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		prey = GameObject.FindGameObjectWithTag("Food");
+		
+		distance = Vector3.Distance(prey.transform.position, transform.position);
+		
+		print(distance);
+		if(distance < audioNearDistance && audio.clip != heartFast)
+		{
+			audio.clip=heartFast;
+			audio.Stop();	
+			
+		}
+		
+		if(distance < audioFarDistance && !audio.isPlaying && distance>audioNearDistance)
+		{
+			audio.clip=heart;
+		 	audio.Play(); 	
+		}
+		
+		else if(distance < audioNearDistance && !audio.isPlaying)
+		{
+			audio.clip=heartFast;
+			audio.Play();
+		}
+		
+	
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+}
